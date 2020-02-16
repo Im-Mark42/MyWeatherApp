2558508d6883bd4ab1444d520a39616cfa6e7567
Sachidananda Sahu <sachi.sujit@gmail.com>
2020-02-16 09:49:23 +0530

    first commit

---
+++ app/src/main/java/com/sach/mark42/myweatherapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sach.mark42.myweatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
